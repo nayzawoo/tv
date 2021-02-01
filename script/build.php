84dff11fb4fd893c0b64ee285310eaccdce37a28
nayzawoo <nayzawoo.me@gmail.com>
2021-02-01 20:24:43 +0630

    update script

---
+++ script/build.php
@@ -0,0 +1,36 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
+
+$data = Yaml::parseFile(__DIR__ . "/../source/channels.yaml");
+$channels = $data['channels'];
+
+$out = '#EXTM3U';
+
+foreach ($channels as $key => $channel) {
+	$logo = isset($channel['logo']) ? $channel['logo'] : 'default.png';
+
+	if (!file_exists(__DIR__ . '/../assets/icons/' . $logo)) {
+		throw new Exception("Logo not found for channel " . $channel['name']);
+	}
+
+	$temp = [
+		"#EXTINF:-1",
+		'tvg-id=""', 
+		'tvg-name=""',
+		'tvg-language="Burmese"', 
+		'tvg-logo="https://raw.githubusercontent.com/nayzawoo/tv/main/assets/icons/'. $logo.'"',
+		'group-title="",'
+	];
+
+	$out .= "\n" . implode(' ', $temp);
+	$out .= $channel['name'];
+	$out .= "\n" . $channel['url'];
+}
+
+file_put_contents(__DIR__ . '/../test.m3u', $out);
+
+echo "\033[32mDone! \n";
+
---
+++ script/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c8a68aedaab1d5a636a74fb9d6157adfb9e47ab6',
+    'name' => 'nayzawoo/tv',
+  ),
+  'versions' => 
+  array (
+    'nayzawoo/tv' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c8a68aedaab1d5a636a74fb9d6157adfb9e47ab6',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa56b4074d1ae755beb55617ddafe6f5d78f665',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v5.2.2',
+      'version' => '5.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6bb8b36c6dea8100268512bf46e858c8eb5c545e',
+    ),
+  ),
+);
